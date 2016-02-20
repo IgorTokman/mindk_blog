25fea2e3cf37ac9a591f7b98c5bbbbf6c6edb797
IgorTokman <igortokman1994@ukr.net>
2016-02-20 22:25:04 +0400

    added empty HttpNotFoundException and WrongResponseTypeException classes and created function dispatch in the Application

--- framework/Application.php
+++ framework/Application.php
@@ -8,6 +8,9 @@
 
 namespace Framework;
 
+use Framework\Exception\HttpNotFoundException;
+use Framework\Exception\WrongResponseTypeException;
+use Framework\Response\Response;
 use Framework\Router\Router;
 
 class Application
@@ -24,5 +27,53 @@ class Application
     {
         $router = new Router(include('../app/config/routes.php'));
         $route =  $router->parseRoute($_SERVER['REQUEST_URI']);
+
+        try{
+            $this->dispatch($route);
+        }
+        catch(HttpNotFoundException $e){
+            // Render 404 or just show msg
+        }
+        catch(\Exception $e){
+            // Render 500 layout or just show msg
+        }
+    }
+
+    //Creates an instance of controller and calls an action of that controller
+    private function dispatch($route){
+        //Checks if route is empty
+        if(!empty($route)) {
+            
+            //Checks if controller class exists
+            if(class_exists($route['controller'])) {
+                $controllerReflection = new \ReflectionClass($route['controller']);
+                $action = $route['action'] . 'Action';
+
+                //Check if controller has action
+                if ($controllerReflection->hasMethod($action)) {
+                    $controller = $controllerReflection->newInstance();
+                    $actionReflection = $controllerReflection->getMethod($action);
+
+                    //Checks if action has the appropriate number of parameters
+                    if ($actionReflection->getNumberOfParameters() <= count($route['params']))
+                        $response = $actionReflection->invokeArgs($controller, $route['params']);
+                    else
+                        throw new \Exception("Does not match the number of parameters");
+
+                    //Checks if the object $response is of the same class or a descendant of the class Response
+                    if ($response instanceof Response) {
+
+                    }
+                    else
+                        throw new WrongResponseTypeException("Controller returns wrong type of Response");
+                }
+                else
+                    throw new \Exception("Controller does not have this action");
+            }
+            else
+                throw new \Exception("Class of controller does not found");
+        }
+        else
+            throw new HttpNotFoundException("Route does not found");
     }
 }
---
+++ framework/Exception/HttpNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IgorTokman
+ * Date: 20.02.2016
+ * Time: 18:32
+ */
+
+namespace Framework\Exception;
+
+
+class HttpNotFoundException extends \Exception
+{
+
+}
