4821ede41a510466ea1814843124bb792a982897
IgorTokman <igortokman1994@ukr.net>
2016-02-26 20:02:46 +0400

    implemented the autoloading of classes by using Composer

--- web/index.php
+++ web/index.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once(__DIR__.'/../framework/Loader.php');
-
-Loader::addNamespacePath('Blog\\',__DIR__.'/../src/Blog');
+$loader = require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new \Framework\Application(__DIR__.'/../app/config/config.php');
 
